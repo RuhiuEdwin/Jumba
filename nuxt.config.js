90d5d7426f4236808fe6204fa4da6b91c7f2eaf5
Ruhiu Edwin <edwin.nganga@roamtech.com>
2022-12-03 22:21:46 +0300

    url change

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://127.0.0.1:8000/api/',
+    // baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: 'https://eddyruhiu.pythonanywhere.com/api/'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
